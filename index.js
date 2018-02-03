b1f64bce1d15ade16e96152b09c3744d13338df4
Kristen Overberg <Kristen@Kristens-MacBook-Pro-2.local>
2018-02-03 11:28:17 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+import 'haul/hot/patch';
+
+import { AppRegistry } from 'react-native';
+import App from './App';
+
+AppRegistry.registerComponent('HaulFish', () => App);
---
+++ webpack.haul.js
@@ -0,0 +1,7 @@
+module.exports = ({ platform }) => ({
+  entry: `./index.js`,
+});
+
+// module.exports = ({ platform }) => ({
+//   entry: `./${platform}/index.${platform}.js`,
+// });
