e436e24a9b34eb567b469248d653fb5114f550ab
Kristen Overberg <Kristen@Kristens-MacBook-Pro-2.local>
2018-02-03 12:34:02 -0800

    Modifying stuff for react-native-navigation

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
+import 'haul/hot/patch';
+import {
+  makeHot,
+  tryUpdateSelf,
+  callOnce,
+  clearCacheFor,
+  redraw
+} from 'haul/hot';
+
+
 import { AppRegistry } from 'react-native';
 import App from './App';
 
-AppRegistry.registerComponent('RnHaulTest', () => App);
+// AppRegistry.registerComponent('RnHaulTest', () => App);
+AppRegistry.registerComponent('RnHaulTest', makeHot(() => App));
+
+if (module.hot) {
+  module.hot.accept(() => {})
+  module.hot.accept('./App', () => {
+    clearCacheFor(require.resolve('./App'));
+    redraw(() => require('./App').default);
+  });
+}
